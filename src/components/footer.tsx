3369022ea6a64f78408a87e2fc9e8e62f0f92433
AmanRaza78 <162736647+JBD-0001@users.noreply.github.com>
2024-03-14 20:04:09 +0530

    testing changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,8 +9,7 @@ const Footer = () => {
           Market Mingle
         </a>
         <p className="my-6 text-gray-500 dark:text-gray-400">
-          Open-source library of over 400+ web components and interactive
-          elements built for better web.
+          A digital marketplace
         </p>
         <ul className="mb-6 flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
           <li>
